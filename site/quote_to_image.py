256e8b2987b6588757ba6075c280ae324d26fcae
Tim Hopper <tdhopper@users.noreply.github.com>
2020-12-18 08:43:36 -0500

    typo

--- site/quote_to_image.py
+++ site/quote_to_image.py
@@ -13,7 +13,7 @@ TEMPLATE = Path("image_template.html")
 USER_ID = os.environ.get('USER_ID').strip()
 API_KEY = os.environ.get('API_KEY').strip()
 
-print(f"""USER_ID: {USER_ID[:5] {USER_ID[-5:]}
+print(f"""USER_ID: {USER_ID[:5]} {USER_ID[-5:]}
 API_KEY: {APIKEY[:5]} {APIKEY[-5:]}""")
 
 def markdown_to_html(content):
