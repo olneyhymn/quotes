622141af2da6d1dca30443318bae4c700f265a1a
Tim Hopper <tdhopper@gmail.com>
2020-12-27 21:14:22 -0500

    fix image rendering

--- site/content/q/2009-04-13-18-36-charles-spurgeon.md
+++ site/content/q/2009-04-13-18-36-charles-spurgeon.md
@@ -3,9 +3,13 @@ authors:
 - Charles Spurgeon
 date: '2009-04-13'
 description: ''
+images:
+- https://hcti.io/v1/image/cd20b6ac-a78e-4436-98af-d2d7b7cff35d
 slug: the-history-of-man
+tags:
+- death
+- life
 title: The History of Man
-tags: ["death", "life"]
 ---
 
 The grass lives, grows, flowers, falls beneath the scythe, dries up, and is removed from the field: read this sentence over again, and you will find it the history of man.
--- site/quote_to_image.py
+++ site/quote_to_image.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import markdown
 import frontmatter
 import requests
@@ -12,44 +14,56 @@ app = typer.Typer()
 TEMPLATE = Path("image_template.html")
 CSS = Path("themes/ezhil/static/css/main.css")
 
+
 def markdown_to_html(content):
     html = markdown.markdown(content.content)
     t = Template(TEMPLATE.read_text())
-    if 'bible_reference' in content:
-        authors = content['bible_reference']
+    if "bible_reference" in content:
+        authors = content["bible_reference"]
     else:
-        authors = ", ".join(content['authors'])
+        authors = ", ".join(content["authors"])
     return t.render(body=html, author=authors)
+
 
 def html_to_image(html: str):
 
-    USER_ID = os.environ['USER_ID']
-    API_KEY = os.environ['API_KEY']
+    USER_ID = os.environ["USER_ID"]
+    API_KEY = os.environ["API_KEY"]
     HCTI_API_ENDPOINT = "https://hcti.io/v1/image"
-    data = { 'html': html, 'selector': 'blockquote', 'ms_delay': 500, 'css': CSS.read_text() }
+    data = {
+        "html": html,
+        "selector": "blockquote",
+        "ms_delay": 1000,
+        "css": CSS.read_text(),
+        "google_fonts": "Lora",
+    }
 
-    image = requests.post(url = HCTI_API_ENDPOINT, data = data, auth=(USER_ID, API_KEY))
+    image = requests.post(url=HCTI_API_ENDPOINT, data=data, auth=(USER_ID, API_KEY))
 
     try:
-        return image.json()['url']
+        return image.json()["url"]
     except KeyError:
         raise RuntimeError(image.json())
 
 
 @app.command()
-def update_images(path: Path, glob="*"):
+def update_images(path: Path, glob="*", force=False):
     for post in path.glob(glob):
         if post.name in ["_index.md"]:
             continue
         content = frontmatter.loads(post.read_text())
-        if 'images' not in content and content.get('draft') is not True:
-            content['images'] = [html_to_image(markdown_to_html(content))]
+        if force is True or (
+            "images" not in content and content.get("draft") is not True
+        ):
+            content["images"] = [html_to_image(markdown_to_html(content))]
             post.write_text(frontmatter.dumps(content))
             print(f"Updated {post}")
+
 
 @app.command()
 def html(path: Path):
     print(markdown_to_html(frontmatter.loads(path.read_text())))
+
 
 if __name__ == "__main__":
     app()
