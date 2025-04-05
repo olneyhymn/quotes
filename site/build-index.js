57a9cf162d7c048e46b1589fe1884f049fab404e
Tim Hopper <tdhopper@gmail.com>
2025-04-04 20:35:01 -0400

    add embeddings to search

--- site/build-index.js
+++ site/build-index.js
@@ -1,7 +1,10 @@
 const fs = require('fs').promises;
 var read = require('fs-readdir-recursive')
-const {promisify} = require('util');
+const { promisify } = require('util');
 const frontMatterParser = require('parser-front-matter');
+// We'll use a simple-transformers library for our embedding needs
+const natural = require('natural');
+const { TfIdf } = natural;
 
 const parse = promisify(frontMatterParser.parse.bind(frontMatterParser));
 
@@ -13,7 +16,7 @@ async function loadPostsWithFrontMatter(postsDirectoryPath) {
         `${postsDirectoryPath}/${fileName}`,
         'utf8'
       );
-      const {content, data} = await parse(fileContent);
+      const { content, data } = await parse(fileContent);
       return {
         content: content,
         ...data,
@@ -28,7 +31,7 @@ async function loadPostsWithFrontMatter(postsDirectoryPath) {
 const lunrjs = require('lunr');
 
 function makeIndex(posts) {
-  return lunrjs(function() {
+  return lunrjs(function () {
     this.ref('title');
     this.field('title');
     this.field('content');
@@ -41,10 +44,51 @@ function makeIndex(posts) {
   });
 }
 
+// Function to create TF-IDF embeddings for fuzzy topic searching
+function createEmbeddings(posts) {
+  const tfidf = new TfIdf();
+
+  // Add documents to the corpus
+  posts.forEach((post, index) => {
+    const docText = `${post.title} ${post.content} ${post.authors} ${post.tags} ${post.description || ''}`;
+    tfidf.addDocument(docText, post.title);
+  });
+
+  // Generate document vectors
+  const documentVectors = {};
+  tfidf.documents.forEach((doc, docIndex) => {
+    const terms = Object.keys(doc);
+    const vector = {};
+    terms.forEach(term => {
+      if (term !== '__key') {
+        vector[term] = tfidf.tfidf(term, docIndex);
+      }
+    });
+    documentVectors[tfidf.documents[docIndex].__key] = vector;
+  });
+
+  return documentVectors;
+}
+
 async function run() {
   const posts = await loadPostsWithFrontMatter(`${__dirname}/content`);
-  const index = makeIndex(posts);
-  console.log(JSON.stringify(index));
+
+  // Create both traditional and embedding-based indices
+  const lunrIndex = makeIndex(posts);
+  const embeddings = createEmbeddings(posts);
+
+  // Combine indices into one output
+  const combinedIndex = {
+    lunrIndex: lunrIndex,
+    embeddings: embeddings,
+    // Include a simple mapping of titles to full post data
+    posts: posts.reduce((acc, post) => {
+      acc[post.title] = post;
+      return acc;
+    }, {})
+  };
+
+  console.log(JSON.stringify(combinedIndex));
 }
 
 run()
--- site/static/search.js
+++ site/static/search.js
@@ -1,61 +1,140 @@
-(function() {
-    function displaySearchResults(results, store) {
-      var searchResults = document.getElementById('search-results');
+(function () {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
 
-      if (results.length) { // Are there any results?
-        var appendString = '';
+    if (results.length) { // Are there any results?
+      var appendString = '';
 
-        for (var i = 0; i < results.length; i++) {  // Iterate over the results
-          var item = store[results[i].ref];
-          appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-          appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-        }
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+      }
 
-        searchResults.innerHTML = appendString;
-      } else {
-        searchResults.innerHTML = '<li>No results found</li>';
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
       }
     }
+  }
 
-    function getQueryVariable(variable) {
-      var query = window.location.search.substring(1);
-      var vars = query.split('&');
+  // Calculate cosine similarity between two vectors
+  function cosineSimilarity(vec1, vec2) {
+    const common_terms = Object.keys(vec1).filter(term => term in vec2);
 
-      for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
+    // If no common terms, return 0
+    if (common_terms.length === 0) return 0;
 
-        if (pair[0] === variable) {
-          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-        }
-      }
+    let dot_product = 0;
+    for (const term of common_terms) {
+      dot_product += vec1[term] * vec2[term];
     }
 
-    var searchTerm = getQueryVariable('query');
+    // Calculate magnitudes
+    let mag1 = 0;
+    let mag2 = 0;
 
-    if (searchTerm) {
-      document.getElementById('search-box').setAttribute("value", searchTerm);
+    for (const val of Object.values(vec1)) {
+      mag1 += val * val;
+    }
 
-      // Initalize lunr with the fields it will be searching on. I've given title
-      // a boost of 10 to indicate matches on this field are more important.
-      var idx = lunr(function () {
-        this.field('id');
-        this.field('title', { boost: 10 });
-        this.field('author');
-        this.field('category');
-        this.field('content');
-      });
+    for (const val of Object.values(vec2)) {
+      mag2 += val * val;
+    }
 
-      for (var key in window.store) { // Add the data to lunr
-        idx.add({
-          'id': key,
-          'title': window.store[key].title,
-          'author': window.store[key].author,
-          'category': window.store[key].category,
-          'content': window.store[key].content
+    mag1 = Math.sqrt(mag1);
+    mag2 = Math.sqrt(mag2);
+
+    // Prevent division by zero
+    if (mag1 === 0 || mag2 === 0) return 0;
+
+    return dot_product / (mag1 * mag2);
+  }
+
+  // Create TF-IDF vector for search query
+  function createQueryVector(query, corpus) {
+    // Simple tokenization with stopwords removal
+    const stopwords = ['a', 'an', 'the', 'and', 'or', 'but', 'in', 'on', 'at', 'to', 'for', 'with', 'by'];
+    const terms = query.toLowerCase().split(/\W+/).filter(term =>
+      term.length > 1 && !stopwords.includes(term)
+    );
+
+    // Create a vector based on term frequency
+    const vector = {};
+    terms.forEach(term => {
+      if (!vector[term]) vector[term] = 0;
+      vector[term] += 1;
+    });
+
+    return vector;
+  }
+
+  // Perform semantic search using embeddings
+  function semanticSearch(query, embeddings) {
+    const queryVector = createQueryVector(query);
+    const results = [];
+
+    // Calculate similarity between query and each document
+    for (const [docId, docVector] of Object.entries(embeddings)) {
+      const similarity = cosineSimilarity(queryVector, docVector);
+      if (similarity > 0) { // Only include results with some relevance
+        results.push({
+          ref: docId,
+          score: similarity
         });
-
-        var results = idx.search(searchTerm); // Get lunr to perform a search
-        displaySearchResults(results, window.store); // We'll write this in the next section
       }
     }
-  })();
+
+    // Sort by similarity score (descending)
+    results.sort((a, b) => b.score - a.score);
+
+    return results;
+  }
+
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Fetch search index JSON which now includes both lunr index and embeddings
+    fetch('/search_index.json').then(response => response.json())
+      .then(data => {
+        // Load lunr index
+        var lunrIndex = lunr.Index.load(data.lunrIndex);
+
+        // Perform traditional search
+        var lunrResults = lunrIndex.search(searchTerm);
+
+        // Perform semantic search if no or few results
+        if (lunrResults.length < 3) {
+          const semanticResults = semanticSearch(searchTerm, data.embeddings);
+
+          // Merge results, ensuring no duplicates
+          const existingRefs = new Set(lunrResults.map(r => r.ref));
+          semanticResults.forEach(result => {
+            if (!existingRefs.has(result.ref)) {
+              lunrResults.push(result);
+            }
+          });
+
+          // Re-sort by score
+          lunrResults.sort((a, b) => b.score - a.score);
+        }
+
+        // Use the store from combined index
+        displaySearchResults(lunrResults, data.posts);
+      });
+  }
+})();
